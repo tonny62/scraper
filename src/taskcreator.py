698b4f11417477a783dbc90ac323b955a16ad436
tonny62 <ton_kriddanai@hotmail.com>
2018-07-18 22:39:55 +0700

    rename task_doer.py to taskdoer.py

--- src/taskcreator.py
+++ src/taskcreator.py
@@ -1,6 +1,8 @@
+import pickle
+
 def get_tasks_test():
     '''Method for testing, use pickled data to test.'''
-    with open("lefttasks.pickle", 'rb') as fin:
+    with open("data/test.pickle", 'rb') as fin:
         tasks = pickle.load(fin)
     return tasks
 
@@ -10,3 +12,4 @@ def get_tasks(filein):
     for line in filein:
         output.append(line.split(",")[2])  # See the data
     return output
+
---
+++ src/taskdoer.py
@@ -0,0 +1,48 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from src.parser import parse
+from tqdm import tqdm
+
+def get_session():
+    s = requests.Session()
+    s.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) \
+                 Gecko/20100101 Firefox/60.0"}
+    return s
+
+def do_task(task, session):
+    '''Send a request to smelink, parse the request and insert into database'''    
+    #print("Doing", task)
+    response = session.get("https://smelink.net/search?text={}".format(task), verify=False) # This line s blocking
+    
+    ## There must be 2 parser: 1. DBD for industry 2. SME-Link for ENG Name
+    result = parse(response.text) # Do parsing
+    result['jpNo'] = task[0]
+    
+    #insert(response) # This line also is blocking
+    return result
+
+class mypbar():
+    def __init__(self, lentask, order):
+        self.pbar = tqdm(total=lentask, position=order)
+
+    def update(self):
+        self.pbar.update()
+
+def process_function(tasks, numthread, order):
+    '''Do tasks using threads'''
+
+    mysession = get_session()
+    progressbar = mypbar(len(tasks), order)
+
+    def callback(future):
+        progressbar.update()
+        #print(future.result()['jpNo'], "is Done") 
+
+
+    with ThreadPoolExecutor(max_workers=numthread) as tpool:
+        futures = [tpool.submit(lambda x:do_task(x, mysession),
+            task) for task in tasks]  # do tasks
+        [item.add_done_callback(callback) for item in futures] # add callback
+        results = [future.result() for future in futures]
+    return results
+
